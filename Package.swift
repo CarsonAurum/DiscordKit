b9edc19a89ba060c20746c1b11cc4d3a4b7dfd9b
Carson R <carsonaurum1@icloud.com>
2025-01-30 12:11:23 -0600

    Initial package setup

--- Package.swift
+++ Package.swift
@@ -1,21 +1,42 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "DiscordKit",
+    platforms: [.iOS(.v18), .macOS(.v15)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DiscordKit",
             targets: ["DiscordKit"]),
     ],
+    dependencies: [
+        // NETWORK
+        .package(url: "https://github.com/swift-server/async-http-client.git", branch: "main"),
+        .package(url: "https://github.com/apple/swift-nio.git", branch: "main"),
+        .package(url: "https://github.com/vapor/websocket-kit.git", branch: "main"),
+        // UTILS
+        .package(url: "https://github.com/apple/swift-atomics.git", branch: "main"),
+        .package(url: "https://github.com/apple/swift-collections.git", branch: "main"),
+        .package(url: "https://github.com/apple/swift-crypto.git", branch: "main"),
+        .package(url: "https://github.com/apple/swift-log.git", branch: "main"),
+        .package(url: "https://github.com/Flight-School/AnyCodable", branch: "master"),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DiscordKit"),
+            name: "DiscordKit",
+            dependencies: [
+                .product(name: "AnyCodable", package: "AnyCodable"),
+                .product(name: "AsyncHTTPClient", package: "async-http-client"),
+                .product(name: "Atomics", package: "swift-atomics"),
+                .product(name: "Collections", package: "swift-collections"),
+                .product(name: "Crypto", package: "swift-crypto"),
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "NIO", package: "swift-nio"),
+                .product(name: "WebSocketKit", package: "websocket-kit"),
+            ],
+            path: "Sources"
+        ),
         .testTarget(
             name: "DiscordKitTests",
             dependencies: ["DiscordKit"]
