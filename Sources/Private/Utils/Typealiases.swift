1086541c58da7f3fcf053cd51de2a1bde36c228a
Carson R <carsonaurum1@icloud.com>
2025-01-30 13:56:24 -0600

    Documentation Update

--- Sources/Private/Utils/Typealiases.swift
+++ Sources/Private/Utils/Typealiases.swift
@@ -8,6 +8,10 @@
 import Foundation
 import AnyCodable
 
+/// Codable, Hashable, Sendable
 internal typealias DiscordModel = Codable & Hashable & Sendable
+/// A tuple containing a JSON encoder and decoder pair
 internal typealias CoderPackage = (encoder: JSONEncoder, decoder: JSONDecoder)
-internal typealias GatewayEventAsyncStream = AsyncStream<GatewayEvent<AnyCodable>>
+/// A stream of gateway events that can be iterated over in an async manner.
+internal typealias GatewayEventAsyncStream =
+    AsyncStream<GatewayEvent<AnyCodable>>
